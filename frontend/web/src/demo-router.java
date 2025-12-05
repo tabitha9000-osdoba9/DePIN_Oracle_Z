f20568988e029dff0574f80bc109e698d5395ba9
tabitha9000-osdoba9 <innokentii.k322218205@rambler.ru>
2025-12-05 10:33:15 +0800

    add UniversalAdapter.ts, util.tsx, EthereumConfig__factory.ts

---
+++ frontend/web/src/demo-router.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\demo-router.java
+// Generated at: 2025-10-25T10:51:31.667Z
---
+++ simple/notification-shared-handler/registry-middleware/util.tsx
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+export const useregistry-shared = () => { return {}; };
